9a8c696d778fe74d0b2bfb62a98b58bfe0ac52a5
Krishna Sah <krishnasah9204@gmail.com>
2025-04-29 00:36:33 +0545

    Initial Form of Banking App

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="en">
+        <main>
+          
+          {children}
+        </main>
+      </html>
+    );
+  }
+  
